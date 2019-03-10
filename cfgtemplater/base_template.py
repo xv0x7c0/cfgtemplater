1cafaf4d1d3543bd550ca6d4e76f2580fd0e5c48
xv0x7c0 <13061441+xv0x7c0@users.noreply.github.com>
2019-03-10 16:59:06 +0100

    initial commit

---
+++ cfgtemplater/base_template.py
@@ -0,0 +1,51 @@
+import os, errno, yaml, re
+
+class BaseTemplate:
+
+    def __init__(self, filepath, mandatory_metadata=[]):
+        self.filepath           = os.path.abspath(filepath)
+        self.filename           = os.path.basename(filepath)
+        self.directory          = os.path.dirname(self.filepath)
+        self.mandatory_metadata = mandatory_metadata
+        
+        self.init_split()
+        self.init()
+
+    def init_split(self):
+        """Split given template in two parts : YAML front matter and JINJA2 part"""
+        r = re.compile("""
+                        \A(?:^---[\n\r])
+                        (?P<yaml>.*)
+                        (?:^---[\n\r])
+                        (?P<template>.*)
+                        """,
+                        re.MULTILINE|re.DOTALL|re.VERBOSE)
+        try:
+            with open(self.filepath, 'r') as f:
+                m = re.match(r, f.read())
+                self.header, self.content = m.group(1,2)
+        except IOError:
+            print(IOError(errno.ENOENT, os.strerror(errno.ENOENT), filepath))
+            raise
+
+    def init(self):
+        """Can be overriden to manage default behavior in child classes"""
+        self.init_yaml()
+        self.init_metadata()
+
+    def init_yaml(self):
+        """Load header as YAML markup"""
+        try:
+            self.yaml = yaml.load(self.header)
+        except yaml.YAMLError as ex:
+            print(ex)
+
+    def init_metadata(self):
+        """Check if declared mandatory metadata are present and initialize
+        instance variables for each key found in YAML header"""
+        for metadata in self.mandatory_metadata:
+            if metadata not in self.yaml.keys():
+                raise yaml.YAMLError("Mandatory metadata not in file")
+            
+        for key, value in self.yaml.items():
+            setattr(type(self), key, value)
---
+++ cfgtemplater/config_template.py
@@ -0,0 +1,47 @@
+from cfgtemplater.base_template import BaseTemplate
+from jinja2.nativetypes import NativeEnvironment
+from jinja2 import StrictUndefined
+from jinja2.loaders import FileSystemLoader
+
+class ConfigTemplate(BaseTemplate):
+    def __init__(self, filepath, mandatory_metadata=[]):
+        super().__init__(filepath, mandatory_metadata)
+
+    def init(self):
+        self.init_yaml()
+        self.init_metadata()
+        self.init_environment()
+        self.init_template()
+        self.init_defaults()
+
+    def init_environment(self):
+        """ Initiliazes Jinja2 environment """
+        self.environment = NativeEnvironment(
+                loader=FileSystemLoader(self.directory),
+                lstrip_blocks=True,
+                trim_blocks=True,
+                undefined=StrictUndefined
+                )
+
+    def init_template(self):
+        """ Initializes Jinja2 template based on pre-defined environment """
+        self.template = self.environment.from_string(self.content)
+
+    def init_defaults(self):
+        """ Initializes a dict of default values based on YAML variables """
+        self.defaults = {}
+        if 'variables' in self.yaml.keys():
+            for variable, attributes in self.yaml['variables'].items():
+                if 'default' in attributes:
+                    self.defaults[variable] = attributes['default']
+
+    def render(self, attributes={}):
+        """ Renders the template, merging user values with default values """
+        variables = self.defaults.copy()
+        variables.update(attributes)
+        return self.template.render(variables)
+
+    def save(self, filename, attributes={}):
+        """ Save rendered template in a file """
+        with open (filename, 'w') as f:
+            f.write(self.render(attributes))
