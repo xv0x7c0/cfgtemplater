8141df92ec24bc6a948187c0f19ea0b99c56009d
xv0x7c0 <13061441+xv0x7c0@users.noreply.github.com>
2019-03-31 18:23:05 +0200

    improve command line test coverage and add exception handling

--- cfgtemplater/command_line.py
+++ cfgtemplater/command_line.py
@@ -15,6 +15,10 @@ import yaml
 from cfgtemplater.config_template import ConfigTemplate
 
 
+class LiteralError(Exception):
+    pass
+
+
 def get_parser():
     """Return CLI parser
     """
@@ -61,41 +65,59 @@ def load_module(filepath):
 
 
 def main():
-    parser = get_parser()
-    args = parser.parse_args()
-
-    template = ConfigTemplate(args.filepath)
-
-    final_variables = template.defaults.copy()
-
-    if args.extensions:
-        for extension in args.extensions:
-            module = load_module(extension)
-            template.load_extension(module)
-
-    if args.yaml:
-        for yaml_file in args.yaml:
-            with open(yaml_file, 'r') as f:
-                yaml_variables = yaml.load(f.read())
-                final_variables.update(yaml_variables)
-
-    if args.cli:
-        cli_variables = {}
-        for k, v in [pair.split("=") for pair in args.cli]:
-            value = ast.literal_eval(v)
-            cli_variables[k] = value
-
-        final_variables.update(cli_variables)
-
     try:
+        parser = get_parser()
+        args = parser.parse_args()
+
+        template = ConfigTemplate(args.filepath)
+
+        final_variables = template.defaults.copy()
+
+        if args.extensions:
+            for extension in args.extensions:
+                module = load_module(extension)
+                template.load_extension(module)
+
+        if args.yaml:
+            for yaml_file in args.yaml:
+                with open(yaml_file, 'r') as f:
+                    yaml_variables = yaml.load(f.read(),
+                                               Loader=yaml.FullLoader)
+                    final_variables.update(yaml_variables)
+
+        if args.cli:
+            cli_variables = {}
+            for k, v in [pair.split("=") for pair in args.cli]:
+                try:
+                    value = ast.literal_eval(v)
+                    cli_variables[k] = value
+                except ValueError:
+                    raise LiteralError("Malformed Python literal \
+                                       '%s' in '%s=%s'" % (k, k, v))
+
+            final_variables.update(cli_variables)
+
         print(template.render(final_variables))
         sys.exit(0)
+
     except jinja2.exceptions.UndefinedError as ex:
         print("ERROR: Variable", ex)
-        sys.exit(1)
+        sys.exit(10)
     except jinja2.exceptions.TemplateNotFound as ex:
         print("ERROR: Subtemplate not found:", ex)
-        sys.exit(2)
+        sys.exit(11)
     except jinja2.exceptions.TemplateAssertionError as ex:
         print("ERROR: Missing filter:", ex)
-        sys.exit(3)
+        sys.exit(12)
+    except jinja2.exceptions.TemplateSyntaxError as ex:
+        print("ERROR: Syntax error:", ex)
+        sys.exit(13)
+    except LiteralError as ex:
+        print("ERROR:", ex)
+        sys.exit(14)
+    except yaml.YAMLError as ex:
+        print("ERROR:", ex)
+        sys.exit(15)
+    except FileNotFoundError as ex:
+        print("ERROR:", ex)
+        sys.exit(16)
---
+++ tests/test_command_line.py
@@ -0,0 +1,113 @@
+import sys
+import unittest
+from unittest.mock import mock_open, patch
+import cfgtemplater.command_line
+import pytest
+
+
+class CommandLineTests(unittest.TestCase):
+
+    def data_ok(self):
+        return """---
+variables:
+  variable1:
+    default: value1
+---
+{{ variable1 }}"""
+
+    def data_missing_variable(self):
+        return """{{ variable1 }}"""
+
+    def data_missing_subtemplate(self):
+        return """{% include 'subtemplate.j2' %}"""
+
+    def data_missing_filter(self):
+        return """{{ 'test' | missing_filter }}"""
+
+    def data_syntax_error(self):
+        return """{% include 'subtemplate.j2 }}"""
+
+    def data_yaml_ok(self):
+        return """---
+variable1: test
+"""
+
+    def data_yaml_nok(self):
+        return """---
+variable1: ][
+"""
+
+    def setup_template(self, template, args):
+        filepath = '/dir/template.cfg'
+        mock = mock_open(read_data=template)
+        with patch('cfgtemplater.base_template.open', mock) as f:
+            with patch.object(sys, 'argv', args):
+                with patch.object(sys, "exit") as exit:
+                    cfgtemplater.command_line.main()
+                    self.out, self.err = self.capsys.readouterr()
+                    self.exit = exit
+
+    @pytest.fixture(autouse=True)
+    def capsys(self, capsys):
+        self.capsys = capsys
+
+    def test_render_ok(self):
+        args = ['cfgtemplater', '/dir/template.cfg']
+        self.setup_template(self.data_ok(), args)
+        self.assertEqual(self.out, 'value1\n')
+
+    def test_return_code_ok(self):
+        args = ['cfgtemplater', '/dir/template.cfg']
+        self.setup_template(self.data_ok(), args)
+        self.assertEqual(self.exit.call_args[0][0], 0)
+
+    def test_return_code_missing_variable(self):
+        args = ['cfgtemplater', '/dir/template.cfg']
+        self.setup_template(self.data_missing_variable(), args)
+        self.assertEqual(self.exit.call_args[0][0], 10)
+
+    def test_return_code_missing_subtemplate(self):
+        args = ['cfgtemplater', '/dir/template.cfg']
+        self.setup_template(self.data_missing_subtemplate(), args)
+        self.assertEqual(self.exit.call_args[0][0], 11)
+
+    def test_return_code_missing_filter(self):
+        args = ['cfgtemplater', '/dir/template.cfg']
+        self.setup_template(self.data_missing_filter(), args)
+        self.assertEqual(self.exit.call_args[0][0], 12)
+
+    def test_return_code_syntax_error(self):
+        args = ['cfgtemplater', '/dir/template.cfg']
+        self.setup_template(self.data_syntax_error(), args)
+        self.assertEqual(self.exit.call_args[0][0], 13)
+
+    def test_cli_variables(self):
+        args = ['cfgtemplater', '-p', "variable1='test'", '/dir/template.cfg']
+        self.setup_template(self.data_missing_variable(), args)
+        self.assertEqual(self.exit.call_args[0][0], 0)
+        self.assertEqual(self.out, 'test\n')
+
+    def test_cli_variables_nok(self):
+        args = ['cfgtemplater', '-p', "variable1=test", '/dir/template.cfg']
+        self.setup_template(self.data_missing_variable(), args)
+        self.assertEqual(self.exit.call_args[0][0], 14)
+
+    def test_yaml_ok(self):
+        args = ['cfgtemplater', '-y', 'variables.yaml', '/dir/template.cfg']
+        mock = mock_open(read_data=self.data_yaml_ok())
+        with patch('cfgtemplater.command_line.open', mock) as f:
+            self.setup_template(self.data_missing_variable(), args)
+        self.assertEqual(self.exit.call_args[0][0], 0)
+        self.assertEqual(self.out, 'test\n')
+
+    def test_yaml_nok(self):
+        args = ['cfgtemplater', '-y', 'variables.yaml', '/dir/template.cfg']
+        mock = mock_open(read_data=self.data_yaml_nok())
+        with patch('cfgtemplater.command_line.open', mock) as f:
+            self.setup_template(self.data_missing_variable(), args)
+            self.assertEqual(self.exit.call_args[0][0], 15)
+
+    def test_yaml_file_nok(self):
+        args = ['cfgtemplater', '-y', 'variables.yaml', '/dir/template.cfg']
+        self.setup_template(self.data_missing_variable(), args)
+        self.assertEqual(self.exit.call_args[0][0], 16)
